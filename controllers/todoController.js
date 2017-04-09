7714a1799c64ac80320ef4649cdff9229addf8c5
Alen-Nikolov <alennikolov95@gmail.com>
2017-04-09 16:14:25 +0300

    someting

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -14,7 +14,7 @@ var Todo = mongoose.model('Todo', todoSchema);
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 module.exports = function (app) {
-    app.get('/', function (req, res) {
+    app.get('/todo', function (req, res) {
         //get data from MongoDB and pass it to the view
         Todo.find({}, function (err, data) {
             if (err) throw err;
@@ -22,7 +22,7 @@ module.exports = function (app) {
         });
     });
 
-    app.post('/', urlencodedParser, function (req, res) {
+    app.post('/todo', urlencodedParser, function (req, res) {
         //get data from the view and add it to MongoDB
         var newTodo = Todo(req.body).save(function (err, data) {
             if (err) throw err;
@@ -30,7 +30,7 @@ module.exports = function (app) {
         })
     });
 
-    app.delete('/:item', function (req, res) {
+    app.delete('/todo:item', function (req, res) {
         //delete the requested item from mongoDb
         Todo.findOne({ item: req.params.item.replace(/\-/g, " ")}).remove(function (err, data) {
             if (err) throw err;
