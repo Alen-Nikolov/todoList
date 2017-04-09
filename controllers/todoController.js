6e0cd69644ee3acf2d0f26cb64814098cabfa0dc
Alen-Nikolov <alennikolov95@gmail.com>
2017-04-09 15:17:29 +0300

    First try to commit

---
+++ controllers/todoController.js
@@ -0,0 +1,41 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb://test:test@ds157380.mlab.com:57380/todo')
+
+//create a schema(how the objects would look like- blueprint)
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+module.exports = function (app) {
+    app.get('/todo', function (req, res) {
+        //get data from MongoDB and pass it to the view
+        Todo.find({}, function (err, data) {
+            if (err) throw err;
+            res.render('todo', { todos: data });
+        });
+    });
+
+    app.post('/todo', urlencodedParser, function (req, res) {
+        //get data from the view and add it to MongoDB
+        var newTodo = Todo(req.body).save(function (err, data) {
+            if (err) throw err;
+            res.json(data);
+        })
+    });
+
+    app.delete('/todo/:item', function (req, res) {
+        //delete the requested item from mongoDb
+        Todo.findOne({ item: req.params.item.replace(/\-/g, " ")}).remove(function (err, data) {
+            if (err) throw err;
+            console.log(data);
+            res.json(data);
+        });
+    });
+};
