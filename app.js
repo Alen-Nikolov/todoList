24457fba397805290bd19cc1beac2e6380af2b27
alen.nikolov <alen.nikolov@imperiaonline.org>
2017-06-02 16:44:34 +0300

    changed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'hbs');
 //static files
 app.use(express.static('./public'));
 var port = process.env.PORT || 3000;
-
+console.log("This console log runs when the server starts and it's added with GIT FLOW");
 app.listen(port);
 
 //fire controllers
